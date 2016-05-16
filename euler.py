77f586d69a069bdc62339e05263915e4deb38090
charlesdgg <cdegorguett@enseirb-matmeca.fr>
2016-05-16 20:58:22 +0200

    maj de euler.py

--- euler.py
+++ euler.py
@@ -1,24 +1,32 @@
 import math as m
 import numpy as np
+import matplotlib.pyplot as plt
 
 def step_euler(y,t,h,f):
-    y[t+h] = y[t] + h*F(y[t],t)
-    return y
+   	return(y + h*f(y,t))
 
 def euler_n_step(y0,t0,N,h,f):
-    y = [0]*N
+    y = np.array([0.0]*N)
     y[0] = y0
-    t = [(t0 + h*i) for i in range(N)]
-    for k in range (N):
-        step_euler(y,t[i],h,f)
-    return y
+    T = np.array([(t0 + h*i) for i in range(N)])
+    for k in range (N-1):
+        y[k+1] = step_euler(y[k],T[k],h,f)
+    return np.array(y)
 
 
-def euler(F,y0,t):
-    N = len(t)
-    y = [0]*N
-    y[0] = y0
-    for i in range(N-1):
-        y[i+1] = y[i]+(t[i+1]-t[i])*F(y[i],t[i])
-    return y
  
+def F(y,t):
+    return y
+
+
+t0 = 0.0
+y0 = 1.0
+N = 100
+h = 0.01
+tf = t0 + N*h
+
+t = np.linspace(t0,tf,N)
+y = euler_n_step(y0,t0,N,h,F)
+plt.plot(t,y,'b-')
+plt.plot(t,np.exp(t),'r--')
+plt.show()
