8046f410b58369867e4634284743fef95c9d848b
DE GORGUETTE Charles <cdegorguett@sautaski.pedago.ipb.fr>
2016-05-20 11:14:52 +0200

    maj euler

--- euler.py
+++ euler.py
@@ -66,8 +66,44 @@ def rungek_n_step(y0,t0,N,h,f):
 		y[k+1] = step_rungek(y[k],T[k],h,f)
 	return np.array(y)
 
+##################### Fonction generiques #####################
+
+def meth_n_step(y0,t0,N,h,f,meth):
+        meth = "step_" + meth
+        y = np.array([0.0]*N)
+        y[0] = y0
+        T = np.array([(t0 + h*i) for i in range(N)])
+        for k in range (N-1):
+                y[k+1] = step_(y[k],T[k],h,f)
+        return np.array(y)
+
+#def meth_epsilon(y0,t0,tf,eps,f,meth):
+#        y = np.array([0.0]*)
+
+
+
+#################### Pendule a 1 maillons
+
+def step_euler2(y,t,h,f):
+   	return np.array(y + h*f(y,t))
+
+def euler2(y0,t0,N,h,f):
+    y = np.array([0.0,0.0]*N)
+    y[0] = y0
+    T = np.array([(t0 + h*i) for i in range(N)])
+    for k in range (N-1):
+        y[k+1] = step_euler2(y[k],T[k],h,f)
+    return np.array(y)
+
+def F2(teta,t):
+        g = 9.81
+        l = 5
+        return(np.array([teta[1],-g*m.sin(teta[0])/l]))
+
+y2 = np.array[0.0,0.0]
+t2 = 0.0
+
 ##################### Tests #####################
- 
 
 def F(y,t):
     return y
@@ -83,11 +119,16 @@ y1 = euler_n_step(y0,t0,N,h,F)
 y2 = pt_milieu_n_step(y0,t0,N,h,F)
 y3 = heun_n_step(y0,t0,N,h,F)
 y4 = rungek_n_step(y0,t0,N,h,F)
+
+y = euler2(y2,t2,N,h,F2)
+plt.plot(t,y[0],'b-')
+
 #plt.plot(t,y1,'b-')
-plt.plot(t,y2,'g-')
+#plt.plot(t,y2,'g-')
 #plt.plot(t,y3,'b-')
-plt.plot(t,y4,'b-')
-plt.plot(t,np.exp(t),'r--')
+#plt.plot(t,y4,'b-')
+
+#plt.plot(t,np.exp(t),'r--')
 plt.show()
 
 
